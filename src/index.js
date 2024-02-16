6c92931d19b3730ed0b3db7c5fd61c8764cb6e9f
Julian Lim Kah Chun <jullim@deloitte.com>
2024-02-16 11:13:11 +0800

    another commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import NoteApp from './components/NoteApp';
+
+
+
+
+
+
+// const App = (props) => {
+//   const [count=0, setCount] = useState(props.count)
+//   const [text, setText] = useState('')
+
+//   useEffect(() => {
+//     console.log('This shud run once')
+//   }, [])
+
+//   useEffect(() => {
+//     console.log('useEffect ran')
+//     document.title = count
+//   }, [count])
+
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={() => setCount(count + 1)}>+1</button>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+//       <button onClick={() => setCount(props.count? props.count:0)}>Reset</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)}/>
+//       </div>
+//   )
+// }
+
+
+// App.defaultProps = {
+//   count: 5
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <NoteApp />
 );
 
 // If you want to start measuring performance in your app, pass a function
